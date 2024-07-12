5f26b439fb0f030b731b0d4de15ea3183123073d
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-07-11 21:51:16 -0300

    Prisma Check Ins Methods

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -11,8 +11,14 @@ export class PrismaUsersRepository implements UsersRepositoryInterface {
     return user;
   }
 
-  findById(id: string): Promise<User | null> {
-    throw new Error("Method not implemented.");
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
   }
 
   async create(data: Prisma.UserCreateInput) {

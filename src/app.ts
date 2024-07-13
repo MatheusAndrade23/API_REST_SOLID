231dfc5c5efd5a60eb69190f15a21d1effc6b9e7
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-07-13 19:27:26 -0300

    Refresh Token

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,31 @@ import fastifyJwt from "@fastify/jwt";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
+import fastifyCookie from "@fastify/cookie";
+import cors from "@fastify/cors";
 
 import { ZodError } from "zod";
 import { env } from "@/env";
 
 export const app = fastify();
 
-app.register(fastifyJwt, {
-  secret: env.JWT_SECRET,
+app.register(cors, {
+  origin: true,
+  credentials: true,
 });
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 app.register(usersRoutes);
 app.register(gymsRoutes);
 app.register(checkInsRoutes);
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export const usersRoutes = async (app: FastifyInstance) => {
   app.post("/users", register);
 
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   // Authenticated routes
 
